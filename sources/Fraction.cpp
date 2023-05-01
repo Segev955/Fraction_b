ccb34aab75287491b75eccf75d181b7b29d177ee
segev <segev@Ubunto.myguest.virtualbox.org>
2023-05-01 15:18:48 +0300

    4 tests not working

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -2,54 +2,31 @@
 
 using namespace ariel;
 
+
+//default
+Fraction::Fraction() : num(0), den(1) {}
+
 //constructor
 Fraction::Fraction(const int &num1, const int &num2) : num(num1), den(num2) {
     if (num2 == 0) {
         throw invalid_argument("Division by zero is undefined");
     }
-//    if (num2 < 0) {
-//        this->num = num1 * -1;
-//        this->den = num2 * -1;
-//    }
-
-//    if (num1 == 0) {
-//        this->den = 1;
-//    }
     reduce();
+    //Minus at the numerator
+    if (this->den < 0) {
+        this->num = this->num * -1;
+        this->den = this->den * -1;
+    }
 }
 
-Fraction::Fraction() : num(0), den(1) {
-
-}
-
-//copy
-Fraction::Fraction(const Fraction &other) : num(other.num), den(other.den) {}
-
-//int
-Fraction::Fraction(int n) : Fraction(n, 1) {}
-
-//double convert
-//Fraction::operator double() const {
-//    return double(num) / double(den);
-//}
-
-//Fraction::operator string() const {
-//    return to_string(num) + "/" + to_string(den);
-//}
 //double constructor
 Fraction::Fraction(double num) {
     setPre(float(num));
     reduce();
 }
 
-//float (for the test)
-Fraction::Fraction(float num) {
-    setPre(float(num));
-    reduce();
-}
-
+//getters:
 int Fraction::getNumerator() const { return this->num; }
-
 int Fraction::getDenominator() const { return this->den; }
 
 //add
@@ -61,26 +38,10 @@ Fraction Fraction::operator+(const Fraction &other) const {
     return Fraction(int(n), int(d));
 }
 
-
-//add float
-Fraction Fraction::operator+(const float &other) const {
-    return *this + Fraction(other);
-}
-
 //add double
 Fraction Fraction::operator+(const double &other) const {
     return *this + Fraction(other);
 }
-
-//add int
-Fraction Fraction::operator+(const int &other) const {
-    return *this + Fraction(other);
-}
-
-//Fraction Fraction::operator+(const double &num1, const double &num2) {
-//    double ans = num1 + num2;
-//    return Fraction(ans);
-//}
 
 //sub
 Fraction Fraction::operator-(const Fraction &other) const {
@@ -91,20 +52,9 @@ Fraction Fraction::operator-(const Fraction &other) const {
     return Fraction(int(n), int(d));
 }
 
-//sub float
-Fraction Fraction::operator-(const float &other) const {
-    return *this - Fraction(other);
-}
-
 //sub double
 Fraction Fraction::operator-(const double &other) const {
     return *this - Fraction(other);
-}
-
-//sub int
-Fraction Fraction::operator-(const int &other) const {
-    return *this - Fraction(other);
-
 }
 
 //mul
@@ -116,20 +66,9 @@ Fraction Fraction::operator*(const Fraction &other) const {
     return Fraction(int(n), int(d));
 }
 
-//mul float
-Fraction Fraction::operator*(const float &other) const {
-    return *this * Fraction(other);
-}
-
 //mul double
 Fraction Fraction::operator*(const double &other) const {
     return *this * Fraction(other);
-}
-
-//mul int
-Fraction Fraction::operator*(const int &other) const {
-    return *this / Fraction(other);
-
 }
 
 //dev
@@ -144,25 +83,9 @@ Fraction Fraction::operator/(const Fraction &other) const {
     return Fraction(int(n), int(d));
 }
 
-//dev float
-Fraction Fraction::operator/(const float &other) const {
-    if (other == 0.0) {
-        throw runtime_error("Division by zero is undefined");
-    }
-    return *this / Fraction(other);
-}
-
 //dev double
 Fraction Fraction::operator/(const double &other) const {
     if (other == 0.0) {
-        throw runtime_error("Division by zero is undefined");
-    }
-    return *this / Fraction(other);
-}
-
-//dev int
-Fraction Fraction::operator/(const int &other) const {
-    if (other == 0) {
         throw runtime_error("Division by zero is undefined");
     }
     return *this / Fraction(other);
@@ -173,28 +96,16 @@ bool Fraction::operator==(const Fraction &other) const {
 //    reduce();
     if (this->den < 0) {
         Fraction fra(this->num * -1, this->den * -1);
-        return fra.num == other.num && fra.den == other.den;
+        return fra.num  == other.num && fra.den == other.den;
 
     }
     return this->num == other.num && this->den == other.den;
-}
-
-//equal float
-bool Fraction::operator==(const float &other) const {
-    Fraction a(other);
-    return this->num == a.num && this->den == a.den;
 }
 
 //equal double
 bool Fraction::operator==(const double &other) const {
     Fraction a(other);
     return this->num == a.num && this->den == a.den;
-}
-
-//equal int
-bool Fraction::operator==(const int &other) const {
-    float n = (float) this->num / this->den;
-    return n == other;
 }
 
 //Bigger
@@ -204,20 +115,8 @@ bool Fraction::operator>(const Fraction &other) const {
     return n > o;
 }
 
-//bigger float
-bool Fraction::operator>(const float &other) const {
-    float n = (float) this->num / this->den;
-    return n > other;
-}
-
 //bigger double
 bool Fraction::operator>(const double &other) const {
-    float n = (float) this->num / this->den;
-    return n > other;
-}
-
-//bigger int
-bool Fraction::operator>(const int &other) const {
     float n = (float) this->num / this->den;
     return n > other;
 }
@@ -229,22 +128,10 @@ bool Fraction::operator<(const Fraction &other) const {
     return n < o;
 }
 
-//Smaller float
-bool Fraction::operator<(const float &other) const {
-    Fraction a(other);
-    return *this < a;
-}
-
 //Smaller double
 bool Fraction::operator<(const double &other) const {
     Fraction a(other);
     return *this < a;
-}
-
-//Smaller int
-bool Fraction::operator<(const int &other) const {
-    float n = (float) this->num / this->den;
-    return n < other;
 }
 
 //Smaller or equal
@@ -254,20 +141,8 @@ bool Fraction::operator<=(const Fraction &other) const {
     return n <= o;
 }
 
-//Smaller or equal (float)
-bool Fraction::operator<=(const float &other) const {
-    float n = (float) this->num / this->den;
-    return n <= other;
-}
-
 //Smaller or equal (double)
 bool Fraction::operator<=(const double &other) const {
-    float n = (float) this->num / this->den;
-    return n <= other;
-}
-
-//Smaller or equal (int)
-bool Fraction::operator<=(const int &other) const {
     float n = (float) this->num / this->den;
     return n <= other;
 }
@@ -279,24 +154,13 @@ bool Fraction::operator>=(const Fraction &other) const {
     return n >= o;
 }
 
-//bigger or equal (float)
-bool Fraction::operator>=(const float &other) const {
-    float n = (float) this->num / this->den;
-    return n >= other;
-}
-
 //bigger or equal (double)
 bool Fraction::operator>=(const double &other) const {
     float n = (float) this->num / this->den;
     return n >= other;
 }
 
-//bigger or equal (int)
-bool Fraction::operator>=(const int &other) const {
-    float n = (float) this->num / this->den;
-    return n >= other;
-}
-
+//Output
 std::ostream &operator<<(std::ostream &output, const Fraction &fraction) {
     int one = 1;
     if (fraction.den < 0)
@@ -305,31 +169,26 @@ std::ostream &operator<<(std::ostream &output, const Fraction &fraction) {
     return output;
 }
 
+//Input
 std::istream &operator>>(std::istream &input, Fraction &fraction) {
     int n, d;
-//    char c;
     if (input >> n >> d) {
         if (d == 0) {
             throw runtime_error("Division by zero is undefined");
         }
-        if (n == 0) {
+        if (n == 0) { //Fraction equal 0
             fraction.num = n;
             fraction.den = 1;
         } else {
             fraction.num = n;
             fraction.den = d;
         }
-//        } else
-//            input.setstate(std::ios_base::failbit);
-
     } else {
         throw runtime_error("Input 2 numbers");
-
     }
 
     return input;
 }
-
 
 Fraction &operator++(Fraction &fra) { //++n
     fra = fra + 1;
@@ -362,18 +221,15 @@ int gcd(int a, int b) {
 
 void Fraction::reduce() {
     int n = gcd(num, den);
-//    if (n < 0) {
-//        n *= -1;
-//    }
     this->num = num / n;
     this->den = den / n;
 }
 
-void Fraction::setPre(float num) {
 //set precision 3 digits
+void Fraction::setPre(float num) {
     std::ostringstream os;
-    os << std::fixed << std::setprecision(3) << num;
-    num = stod(os.str());
+    os << std::fixed << std::setprecision(3) << num; //to string
+    num = stod(os.str()); //back to float
     int d = 1;
     while (std::ceil(num * d != std::floor(num * d))) {
         d *= 10;
@@ -382,6 +238,7 @@ void Fraction::setPre(float num) {
     this->den = d;
 }
 
+//overflows:
 bool Fraction::overAdd(const Fraction &fra1, const Fraction &fra2) const {
     int max_int = std::numeric_limits<int>::max();
     int min_int = std::numeric_limits<int>::min();
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -21,97 +21,82 @@ private:
     int num;
     int den;
 public:
+//constructor:
     Fraction();
     Fraction(const int &num1,const int &num2);
-    Fraction(int n);
-    Fraction(const Fraction &other);
-    Fraction(double num);
-    Fraction(float num);
+    Fraction(double num); //double
 //getters:
     int getNumerator() const;
     int getDenominator() const;
 
+    //Add
     Fraction operator+(const Fraction &other) const;
-    Fraction operator+(const float &other) const;
     Fraction operator+(const double &other) const;
-    Fraction operator+(const int &other) const;
     friend Fraction operator+(const double &num , const Fraction &other)
     {return Fraction(num) + other;}
 
-
+    //Sub
     Fraction operator-(const Fraction &other) const;
-    Fraction operator-(const float &other) const;
     Fraction operator-(const double &other) const;
-    Fraction operator-(const int &other) const;
     friend Fraction operator-(const double &num , const Fraction &other)
     {return Fraction(num) - other;}
 
+    //Mul
     Fraction operator*(const Fraction &other) const;
-    Fraction operator*(const float &other) const;
     Fraction operator*(const double &other) const;
-    Fraction operator*(const int &other) const;
     friend Fraction operator*(const double &num , const Fraction &other)
     {return Fraction(num) * other;}
 
+    //Dev
     Fraction operator/(const Fraction &other) const;
-    Fraction operator/(const float &other) const;
     Fraction operator/(const double &other) const;
-    Fraction operator/(const int &other) const;
     friend Fraction operator/(const double &num , const Fraction &other)
     {return Fraction(num) / other;}
 
-
+    //Equal
     bool operator==(const Fraction &other) const;
-    bool operator==(const float &other) const;
     bool operator==(const double &other) const;
-    bool operator==(const int &other) const;
     friend bool operator==(const double &num,const Fraction &other)
     {return Fraction(num) == other;}
 
+    //Bigger
     bool operator>(const Fraction &other) const;
-    bool operator>(const float &other) const;
     bool operator>(const double &other) const;
-    bool operator>(const int &other) const;
     friend bool operator>(const double &num,const Fraction &other)
     {return Fraction(num) > other;}
 
+    //Smaller
     bool operator<(const Fraction &other) const;
-    bool operator<(const float &other) const;
     bool operator<(const double &other) const;
-    bool operator<(const int &other) const;
     friend bool operator<(const double &num,const Fraction &other)
     {return Fraction(num) < other;}
 
+
     bool operator<=(const Fraction &other) const;
-    bool operator<=(const float &other) const;
     bool operator<=(const double &other) const;
-    bool operator<=(const int &other) const;
     friend bool operator<=(const double &num,const Fraction &other)
     {return Fraction(num) <= other;}
 
     bool operator>=(const Fraction &other) const;
-    bool operator>=(const float &other) const;
     bool operator>=(const double &other) const;
-    bool operator>=(const int &other) const;
     friend bool operator>=(const double &num,const Fraction &other)
     {return Fraction(num) >= other;}
 
+    //Output
     friend std::ostream &operator<<(std::ostream &output, const Fraction &fraction);
-
+    //Input
     friend std::istream &operator>>(std::istream &input, Fraction &fraction);
 
     friend Fraction &operator++(Fraction &fra); //++n
     friend const Fraction operator++(Fraction &fra, int val); //n++
-
-
-
     friend Fraction &operator--(Fraction &fra); //--n
     friend const Fraction operator--(Fraction &fra, int val); //n--
 
-//    operator double() const; //convert to double
     operator string() const;
+    //Helpers
     void setPre(float num);
     void reduce();
+    //Overflow
     bool overAdd(const Fraction &fra1, const Fraction &fra2) const;
     bool overSub(const Fraction &fra1, const Fraction &fra2) const;
     bool overMul(const Fraction &fra1, const Fraction &fra2) const;
